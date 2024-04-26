66d4756d34d189fcea68d598f1f3561a57cd7965
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2024-04-26 14:35:43 +0200

    fix: add user_ta header

---
+++ videoTEE/ta/user_ta_header_defines.h
@@ -0,0 +1,14 @@
+#ifndef USER_TA_HEADER_DEFINES_H
+#define USER_TA_HEADER_DEFINES_H
+
+#include <video_tee_ta.h>
+
+#define TA_UUID TA_VIDEO_TEE_UUID
+
+#define TA_FLAGS TA_FLAG_SINGLE_INSTANCE
+
+/* Stack and heap size for TA */
+#define TA_STACK_SIZE (2 * 1024)
+#define TA_DATA_SIZE  (32 * 1024)
+
+#endif // !USER_TA_HEADER_DEFINES_H
