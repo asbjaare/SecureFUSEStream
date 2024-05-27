ddceefc17027fe5b1af83512728b4dac131ab678
aaa040 <aaa040@uit.no>
2024-05-27 18:18:30 +0200

    Added fuse

---
+++ fuse/src/main.rs
@@ -0,0 +1,59 @@
+//
+// Copyright (c) 2016-2022 by William R. Fraser
+//
+
+#![deny(rust_2018_idioms)]
+
+use std::env;
+use std::ffi::{OsStr, OsString};
+
+#[macro_use]
+extern crate log;
+
+mod libc_extras;
+mod libc_wrappers;
+mod passthrough;
+
+struct ConsoleLogger;
+
+impl log::Log for ConsoleLogger {
+    fn enabled(&self, _metadata: &log::Metadata<'_>) -> bool {
+        true
+    }
+
+    fn log(&self, record: &log::Record<'_>) {
+        println!("{}: {}: {}", record.target(), record.level(), record.args());
+    }
+
+    fn flush(&self) {}
+}
+
+static LOGGER: ConsoleLogger = ConsoleLogger;
+
+fn main() {
+    log::set_logger(&LOGGER).unwrap();
+    log::set_max_level(log::LevelFilter::Error);
+
+    let args: Vec<OsString> = env::args_os().collect();
+
+    if args.len() != 3 {
+        println!(
+            "usage: {} <target> <mountpoint>",
+            &env::args().next().unwrap()
+        );
+        std::process::exit(-1);
+    }
+
+    let filesystem = passthrough::PassthroughFS {
+        target: args[1].clone(),
+    };
+
+    let fuse_args = [OsStr::new("-o"), OsStr::new("fsname=passthrufs")];
+
+    fuse_mt::mount(
+        fuse_mt::FuseMT::new(filesystem, 1),
+        &args[2],
+        &fuse_args[..],
+    )
+    .unwrap();
+}
